bf52b22d793f721851fe8d6ae0cca9cbf98b2a3a
刘炜龙 <weilong.liu@ubi001.com>
2016-06-14 11:45:17 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Hello App!</h1>
+    <h1>Hello App!1</h1>
     <p>
       <!-- 使用指令 v-link 进行导航。 -->
       <a v-link="{ path: '/foo' }">Go to Foo</a>
@@ -12,12 +12,12 @@
 </template>
 
 <script>
-import Header from  './components/Header'
+// import Header from  './components/Header'
 
-export default {
-  components: {
-    Header
-  }
-}
+// export default {
+//   components: {
+//     Header
+//   }
+// }
 </script>
 
